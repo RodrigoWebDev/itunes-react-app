17be529103293d66feb2f1bf3393fab041f02c6d
rodrigowebdev <rodrigo.queiroz.chagas@gmail.com>
2020-03-06 21:45:15 -0300

    API adjustment

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,77 +2,78 @@
 import React, { Component } from "react"
 import axios from "axios"
 import Card from "../components/Card"
-import Helmet from 'react-helmet'
-import Hero from '../components/Hero'
-
+import Helmet from "react-helmet"
+import Hero from "../components/Hero"
 
 class IndexPage extends Component {
+  setLoader = action => this.setState({ isFetching: action })
 
-    setLoader = (action) => this.setState({ isFetching: action });
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            info: [],
-            query: ''
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      info: [],
+      query: "",
     }
+  }
 
-    handleChange = e => {
-        let value = e.target.value;
+  handleChange = e => {
+    let value = e.target.value
+    this.setState({
+      query: value,
+    })
+  }
+
+  handleClick = e => {
+    this.setLoader(true)
+
+    let queryString = this.state.query
+      .trim()
+      .split(" ")
+      .join("+")
+      .toLowerCase()
+
+    axios
+      .get(`https://itunes.apple.com/search?term=${queryString}`)
+      .then(response => {
         this.setState({
-            query: value
+          info: response.data.results,
         })
-    }
 
-    handleClick = e => {
-        this.setLoader(true);
+        this.setLoader(false)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 
-        let queryString = this.state.query.trim().split(' ').join("+").toLowerCase();
+  render() {
+    const isFetching = this.state.isFetching
 
-        axios.get(`http://itunes.apple.com/search?term=${queryString}`)
-            .then((response) => {
-                this.setState({
-                    info: response.data.results
-                });
+    return (
+      <>
+        <Helmet>
+          <title>Itunes Search App</title>
+        </Helmet>
+        <div className="container">
+          <Hero
+            isFetching={isFetching}
+            handleClick={this.handleClick}
+            handleChange={this.handleChange}
+          />
 
-                this.setLoader(false);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
-
-    render() {
-        const isFetching = this.state.isFetching;
-
-        return (
-            <>
-                <Helmet>
-                    <title>Itunes Search App</title>
-                </Helmet>
-                <div className="container">
-                    <Hero 
-                        isFetching={isFetching}
-                        handleClick={this.handleClick}
-                        handleChange={this.handleChange}
-                    />
-
-                    {!isFetching &&
-                        <ul>
-                            {
-                                this.state.info.map(i => (
-                                    <li key={i.trackId}>
-                                        <Card info={i} />
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    }
-                </div>
-            </>
-        )
-    }
+          {!isFetching && (
+            <ul>
+              {this.state.info.map(i => (
+                <li key={i.trackId}>
+                  <Card info={i} />
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </>
+    )
+  }
 }
 
 export default IndexPage
